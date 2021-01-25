c0d9ac720fb0489177b6f93bfee5e9ce3a6c751f
Matthias Vandenberghe <42861463+MVDB1984@users.noreply.github.com>
2021-01-25 10:29:02 +0100

    username updated code to fetch the username

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./styles.css";
+import React, { useState, useEffect } from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+export default function App({ login }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setUser);
+  }, [login]);
+
+  if (user.name) {
+    return (
+      <div>
+        <h1>Welkom {user.name}</h1>
+      </div>
+    );
+  }
+  return <div className="App">Please loging with a valid user!</div>;
 }
